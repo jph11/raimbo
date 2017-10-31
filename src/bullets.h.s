152c45b8b3e067c2f5e1c3c8ff98fe6b71adb718
Julia García Martínez <julia.garcia.mtnz@gmail.com>
2017-10-31 19:09:02 +0100

    Sprite araña

--- src/bullets.h.s
+++ src/bullets.h.s
@@ -3,10 +3,10 @@
 ;; BULLETS PUBLIC FUNCTIONS
 ;;========================
 ;;========================
-.globl bullets_updateBullets
-.globl bullet_checkCollision
 .globl bullets_draw
 .globl bullets_erase
 .globl bullets_newBullet
 .globl bullets_deleteAllBullets
 .globl drawBullet
+.globl bullets_updateBullets
+.globl bullet_checkCollision
